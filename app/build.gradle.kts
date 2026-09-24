f41fb1bffd6079c69dd55f63339c19cb4fb8d7f7
Takuma Yamada <takuma.yamada22@gmail.com>
2026-09-25 03:40:04 +0900

    Bump version to 1.2.0 (code 14)

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -15,8 +15,8 @@ android {
         applicationId = "com.github.tyamada.MihirakiPDFViewer_Android"
         minSdk = 26
         targetSdk = 37
-        versionCode = 13
-        versionName = "1.1.1"
+        versionCode = 14
+        versionName = "1.2.0"
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables.useSupportLibrary = true
     }
